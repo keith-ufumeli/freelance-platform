27b7ed6938c1ec603b1bc14bc15e92b59c8f8834
tatendabakozw <tatendabakozw@gmail.com>
2021-10-09 06:00:56 +0200

    feat: contracts page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Register from './pages/Auth/Register';
 import Login from './pages/Auth/Login'
 import Home from './pages/home/Home';
 import MyJobs from './pages/MyJobs/MyJobs';
+import Contracts from './pages/Contracts/Contracts';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path = '/register' component={Register} />
         <Route path = '/login' component={Login} />
         <Route path = '/myjobs' component={MyJobs} />
+        <Route path = '/contracts' component={Contracts} />
         <Route exact path="/" component={Home} />
       </Switch>
     </BrowserRouter>
---
+++ src/pages/Contracts/Contracts.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ContractsLayout from '../../layouts/ContractsLayout'
+
+function Contracts() {
+    return (
+        <ContractsLayout>
+            you do not have any contracts at the moment
+        </ContractsLayout>
+    )
+}
+
+export default Contracts
