4ff692a4fc3f01700aef09ad1a670de001a6c0d2
tatendabakozw <tatendabakozw@gmail.com>
2021-10-11 01:22:26 +0200

    feat: add new urls for api'

--- src/utils/apiUrl.js
+++ src/utils/apiUrl.js
@@ -1,2 +1,4 @@
-export const apiUrl = 'http://localhost:5500/api/v1'
-export const socketUrl = 'http://localhost:5500'
+// export const apiUrl = 'http://localhost:5500/api/v1'
+export const apiUrl = 'https://daypitch1.herokuapp.com/api/v1'
+// export const socketUrl = 'http://localhost:5500'
+export const socketUrl = `https://daypitch1.herokuapp.com`
