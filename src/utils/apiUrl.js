a3bc311e10efa5e6b2dbcedea567ee5c5e837717
tatendabakozw <tatendabakozw@gmail.com>
2021-10-12 05:51:26 +0200

    feat: react to a contract

--- src/utils/apiUrl.js
+++ src/utils/apiUrl.js
@@ -1,4 +1,4 @@
-export const apiUrl = 'http://localhost:5500/api/v1'
-// export const apiUrl = 'https://daypitch1.herokuapp.com/api/v1'
-export const socketUrl = 'http://localhost:5500'
-// export const socketUrl = `https://daypitch1.herokuapp.com`
+// export const apiUrl = 'http://localhost:5500/api/v1'
+export const apiUrl = 'https://daypitch1.herokuapp.com/api/v1'
+// export const socketUrl = 'http://localhost:5500'
+export const socketUrl = `https://daypitch1.herokuapp.com`
