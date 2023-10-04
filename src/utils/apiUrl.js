e33e5e536d3d576414d925be9649d62cc09f2525
Keith Ufumeli <94365600+keith-ufumeli@users.noreply.github.com>
2023-10-04 08:16:16 +0200

    Update apiUrl.js

--- src/utils/apiUrl.js
+++ src/utils/apiUrl.js
@@ -1,4 +1,10 @@
 // export const apiUrl = 'http://localhost:5500/api/v1'
-export const apiUrl = 'https://daypitch1.herokuapp.com/api/v1'
+
+// export const apiUrl = 'https://daypitch1.herokuapp.com/api/v1'
+
+export const apiUrl = "https://daypitch.onrender.com/api/v1"
+
 // export const socketUrl = 'http://localhost:5500'
-export const socketUrl = `https://daypitch1.herokuapp.com`
+// export const socketUrl = `https://daypitch1.herokuapp.com`
+
+export const secketUrl = "https://daypitch.onrender.com"
